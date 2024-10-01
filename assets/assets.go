eb0236b5059fad5bdb3ac11317f3bbc894e89266
axzilla <chanter-acidic-0r@icloud.com>
2024-10-01 17:12:21 +0200

    Initial commit

---
+++ assets/assets.go
@@ -0,0 +1,6 @@
+package assets
+
+import "embed"
+
+//go:embed css/* img/*
+var Assets embed.FS
---
+++ internals/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	GoEnv string
+}
+
+var AppConfig *Config
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	} else {
+		log.Println(".env file initialized.")
+	}
+
+	AppConfig = &Config{
+		GoEnv: os.Getenv("GO_ENV"),
+	}
+}
