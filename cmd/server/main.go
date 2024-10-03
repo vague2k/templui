655cf1ccd6add7f5f86a39f788e309faeb58cc29
axzilla <chanter-acidic-0r@icloud.com>
2024-10-03 18:56:28 +0200

    feat: add tabs components and showcase

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,6 +19,7 @@ func main() {
 	mux.Handle("GET /", templ.Handler(pages.HeaderShowcase()))
 	mux.Handle("GET /docs/components/button", templ.Handler(pages.Button()))
 	mux.Handle("GET /docs/components/sheet", templ.Handler(pages.Sheet()))
+	mux.Handle("GET /docs/components/tabs", templ.Handler(pages.TabsShowcase()))
 
 	fmt.Println("Server is running on http://localhost:8090")
 	http.ListenAndServe(":8090", mux)
