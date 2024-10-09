e01eafb674c354172ef09c190a6d66651e993f70
“axzilla” <hello@axeladrian.com>
2024-10-09 18:08:26 +0200

    change: renaming some stuff

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -8,17 +8,17 @@ import (
 
 // CSS returns a link tag for the Goilerplate CSS
 // If no branch is specified, it defaults to 'main'
-func CSS(branch string) templ.Component {
+func GoilerplateCSS(branch string) templ.Component {
 	if branch == "" {
 		branch = "main"
 	}
 
-	cssURL := fmt.Sprintf("https://cdn.jsdelivr.net/gh/axzilla/goilerplate@%s/pkg/styles/goilerplate.css", branch)
+	cssURL := fmt.Sprintf("https://cdn.jsdelivr.net/gh/axzilla/goilerplate@%s/assets/css/goilerplate.css", branch)
 	return templ.Raw(fmt.Sprintf(`<link rel="stylesheet" href="%s">`, cssURL))
 }
 
 // Alpine returns script tags for Alpine.js
-func Alpine() templ.Component {
+func AlpineJs() templ.Component {
 	return templ.Raw(`
         <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
