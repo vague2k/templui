1f325f0ba9892b006e577621b92f69f7b1f84f80
axzilla <hello@axeladrian.com>
2024-12-07 10:36:42 +0700

    fix(tailwind): remove undefined custom sans font family configuration to bring back intellisense

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,9 +54,7 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      fontFamily: {
-        sans: ["Inter", ...fontFamily.sans],
-      },
+      fontFamily: {},
     },
   },
 };
