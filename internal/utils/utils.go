738f84e7d47f0a23c2e2331f7e04cf6a71580423
Arthur Wallendorff <howdy@wallenart.dev>
2025-02-08 13:53:27 +0100

    chore: add retry mechanism + remove TODO in utils function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -8,11 +8,19 @@ import (
 type CtxKey string
 
 const CtxURLPathValueKey = CtxKey("url_value")
+
 func GenerateNonce() string {
+	try := 0
+Retry:
+	try++
+
 	nonceBytes := make([]byte, 16)
 	_, err := rand.Read(nonceBytes)
 	if err != nil {
-		// TODO: Handle the error appropriately
+		if try < 2 {
+			goto Retry
+		}
+
 		return ""
 	}
 	return base64.StdEncoding.EncodeToString(nonceBytes)
