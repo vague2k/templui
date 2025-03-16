1f14580ded4546a6d4d3a565895143e589645e05
axzilla <hello@axeladrian.com>
2025-03-16 18:00:16 +0700

    feat(tooltip/breaking): use composition

--- components/tooltip_templ.go
+++ components/tooltip_templ.go
@@ -11,6 +11,7 @@ import templruntime "github.com/a-h/templ/runtime"
 import "github.com/axzilla/templui/utils"
 
 type TooltipSide string
+type TooltipVariant string
 
 const (
 	TooltipTop    TooltipSide = "top"
@@ -19,8 +20,6 @@ const (
 	TooltipLeft   TooltipSide = "left"
 )
 
-type TooltipVariant string
-
 const (
 	TooltipDefault     TooltipVariant = "default"
 	TooltipSecondary   TooltipVariant = "secondary"
@@ -28,71 +27,26 @@ const (
 )
 
 type TooltipProps struct {
-	Trigger    templ.Component
-	Content    templ.Component
 	Side       TooltipSide
-	ShowArrow  bool
 	Variant    TooltipVariant
 	Class      string
 	Attributes templ.Attributes
 }
 
-func getTooltipSideClass(side TooltipSide) string {
-	switch side {
-	case TooltipTop:
-		return "bottom-full left-1/2 -translate-x-1/2 mb-2"
-	case TooltipRight:
-		return "left-full top-1/2 -translate-y-1/2 ml-2"
-	case TooltipBottom:
-		return "top-full left-1/2 -translate-x-1/2 mt-2"
-	case TooltipLeft:
-		return "right-full top-1/2 -translate-y-1/2 mr-2"
-	default:
-		return "bottom-full left-1/2 -translate-x-1/2 mb-2"
-	}
+type TooltipTriggerProps struct {
+	Class      string
+	Attributes templ.Attributes
 }
 
-func getTooltipVariantClass(variant TooltipVariant) string {
-	switch variant {
-	case TooltipDefault:
-		return "bg-bg text-primary-foreground"
-	case TooltipSecondary:
-		return "bg-secondary text-secondary-foreground"
-	case TooltipDestructive:
-		return "bg-destructive text-destructive-foreground"
-	default:
-		return "bg-foreground text-background"
-	}
+type TooltipContentProps struct {
+	ShowArrow  bool
+	Side       TooltipSide    // Override parent side if needed
+	Variant    TooltipVariant // Override parent variant if needed
+	Class      string
+	Attributes templ.Attributes
 }
 
-func getArrowClass(side TooltipSide) string {
-	switch side {
-	case TooltipTop:
-		return "bottom-[-4px] left-1/2 -translate-x-1/2"
-	case TooltipRight:
-		return "left-[-4px] top-1/2 -translate-y-1/2"
-	case TooltipBottom:
-		return "top-[-4px] left-1/2 -translate-x-1/2"
-	case TooltipLeft:
-		return "right-[-4px] top-1/2 -translate-y-1/2"
-	default:
-		return "bottom-[-4px] left-1/2 -translate-x-1/2"
-	}
-}
-
-func getArrowColor(variant TooltipVariant) string {
-	switch variant {
-	case TooltipDefault:
-		return "bg-foreground"
-	case TooltipSecondary:
-		return "bg-secondary"
-	case TooltipDestructive:
-		return "bg-destructive"
-	default:
-		return "bg-foreground"
-	}
-}
-
+// Main tooltip container that creates the hover effect
 func Tooltip(props TooltipProps) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -114,30 +68,12 @@ func Tooltip(props TooltipProps) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"relative inline-block group\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = props.Trigger.Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		var templ_7745c5c3_Var2 = []any{
-			utils.TwMerge(
-				"absolute w-max z-50 pointer-events-none",
-				"text-xs px-4 py-1 rounded-lg",
-				"transition-opacity duration-200 delay-500 opacity-0 group-hover:opacity-100",
-				"whitespace-nowrap",
-				getTooltipSideClass(props.Side),
-				getTooltipVariantClass(props.Variant),
-				props.Class,
-			),
-		}
+		var templ_7745c5c3_Var2 = []any{utils.TwMerge("relative inline-block group", props.Class)}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div class=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -150,50 +86,249 @@ func Tooltip(props TooltipProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		if props.ShowArrow {
-			var templ_7745c5c3_Var4 = []any{
-				utils.TwMerge(
-					"absolute h-2 w-2 rotate-45",
-					getArrowClass(props.Side),
-					getArrowColor(props.Variant),
-				),
-			}
-			templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var4...)
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<div class=\"")
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
-			var templ_7745c5c3_Var5 string
-			templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var4).String())
-			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/tooltip.templ`, Line: 1, Col: 0}
-			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "\"></div>")
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
-		}
-		templ_7745c5c3_Err = props.Content.Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = templ.RenderAttributes(ctx, templ_7745c5c3_Buffer, props.Attributes)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "</div></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, ">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		return nil
 	})
+}
+
+// Element that triggers the tooltip
+func TooltipTrigger(props TooltipTriggerProps) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var4 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var4 == nil {
+			templ_7745c5c3_Var4 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		var templ_7745c5c3_Var5 = []any{utils.TwMerge("inline-block", props.Class)}
+		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var5...)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<div class=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var6 string
+		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var5).String())
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/tooltip.templ`, Line: 1, Col: 0}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ.RenderAttributes(ctx, templ_7745c5c3_Buffer, props.Attributes)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, ">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var4.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "</div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return nil
+	})
+}
+
+// The actual tooltip content
+func TooltipContent(props TooltipContentProps) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var7 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var7 == nil {
+			templ_7745c5c3_Var7 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		var templ_7745c5c3_Var8 = []any{utils.TwMerge(
+			"absolute w-max z-50 pointer-events-none",
+			"text-xs px-4 py-1 rounded-lg",
+			"transition-opacity duration-200 delay-500 opacity-0 group-hover:opacity-100",
+			"whitespace-nowrap",
+			getTooltipSideClass(props.Side),
+			getTooltipVariantClass(props.Variant),
+			props.Class,
+		)}
+		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var8...)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "<div class=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var9 string
+		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var8).String())
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/tooltip.templ`, Line: 1, Col: 0}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ.RenderAttributes(ctx, templ_7745c5c3_Buffer, props.Attributes)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, ">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if props.ShowArrow {
+			var templ_7745c5c3_Var10 = []any{utils.TwMerge(
+				"absolute h-2 w-2 rotate-45",
+				getArrowClass(props.Side),
+				getArrowColor(props.Variant),
+			)}
+			templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var10...)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<div class=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var11 string
+			templ_7745c5c3_Var11, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var10).String())
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/tooltip.templ`, Line: 1, Col: 0}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var11))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 13, "\"></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var7.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 14, "</div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return nil
+	})
+}
+
+// Helper functions for class generation
+func getTooltipSideClass(side TooltipSide) string {
+	switch side {
+	case TooltipTop:
+		return "bottom-full left-1/2 -translate-x-1/2 mb-2"
+	case TooltipRight:
+		return "left-full top-1/2 -translate-y-1/2 ml-2"
+	case TooltipBottom:
+		return "top-full left-1/2 -translate-x-1/2 mt-2"
+	case TooltipLeft:
+		return "right-full top-1/2 -translate-y-1/2 mr-2"
+	default:
+		return "bottom-full left-1/2 -translate-x-1/2 mb-2" // Default to top
+	}
+}
+
+func getTooltipVariantClass(variant TooltipVariant) string {
+	switch variant {
+	case TooltipDefault:
+		return "bg-foreground text-background"
+	case TooltipSecondary:
+		return "bg-secondary text-secondary-foreground"
+	case TooltipDestructive:
+		return "bg-destructive text-destructive-foreground"
+	default:
+		return "bg-foreground text-background" // Default
+	}
+}
+
+func getArrowClass(side TooltipSide) string {
+	switch side {
+	case TooltipTop:
+		return "bottom-[-4px] left-1/2 -translate-x-1/2"
+	case TooltipRight:
+		return "left-[-4px] top-1/2 -translate-y-1/2"
+	case TooltipBottom:
+		return "top-[-4px] left-1/2 -translate-x-1/2"
+	case TooltipLeft:
+		return "right-[-4px] top-1/2 -translate-y-1/2"
+	default:
+		return "bottom-[-4px] left-1/2 -translate-x-1/2" // Default to top
+	}
+}
+
+func getArrowColor(variant TooltipVariant) string {
+	switch variant {
+	case TooltipDefault:
+		return "bg-foreground"
+	case TooltipSecondary:
+		return "bg-secondary"
+	case TooltipDestructive:
+		return "bg-destructive"
+	default:
+		return "bg-foreground" // Default
+	}
 }
 
 var _ = templruntime.GeneratedTemplate
