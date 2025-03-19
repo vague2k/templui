3ae00c001716b1238f6bff699fe6208f435dddc1
axzilla <hello@axeladrian.com>
2025-03-19 19:48:06 +0700

    feat(charts): rethink WIP

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"math/rand"
 
@@ -51,4 +53,43 @@ func MergeAttributes(attrs ...templ.Attributes) templ.Attributes {
 // Example: "id-123456"
 func RandomID() string {
 	return fmt.Sprintf("id-%d", rand.Intn(1000000))
+}
+
+// create a ifelse func withb bool prop but return any
+func IfElse(b bool, t, f any) any {
+	if b {
+		return t
+	}
+	return f
+}
+
+// float to string create one with standard lib
+func FloatToString(f float64) string {
+	return strconv.FormatFloat(f, 'f', -1, 64)
+}
+
+func BoolToString(b bool) string {
+	if b {
+		return "true"
+	}
+	return "false"
+}
+
+// int to string create one with standard lib
+func IntToString(i int) string {
+	return strconv.Itoa(i)
+}
+
+// json stringify create one with standard lib
+func JSONStringify(v any) string {
+	jsonBytes, err := json.Marshal(v)
+	if err != nil {
+		return "[]" // Rückgabe eines leeren Arrays/Objekts bei Fehler
+	}
+	return string(jsonBytes)
+}
+
+// Ptr returns a pointer to the given value of any type.
+func Ptr[T any](v T) *T {
+	return &v
 }
