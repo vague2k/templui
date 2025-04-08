8d6bd4269139a8b4d4ae4dc061f2d05cca0c3b21
axzilla <hello@axeladrian.com>
2025-04-08 21:45:02 +0700

    feat(carousel): add Carousel component with subcomponents (Content, Item, Previous, Next, Indicators) and associated script for functionality

--- helpers/helpers_templ.go
+++ helpers/helpers_templ.go
@@ -8,7 +8,10 @@ package helpers
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import "github.com/axzilla/templui/components"
+import (
+	"github.com/axzilla/templui/components"
+	"github.com/axzilla/templui/components/carousel"
+)
 
 // ComponentScripts returns script tags for all components.
 func ComponentScripts() templ.Component {
@@ -32,7 +35,7 @@ func ComponentScripts() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = components.CarouselScript().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = carousel.Script().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
