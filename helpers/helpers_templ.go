7dae14c4e9fb4588e23647af229f020855d39455
axzilla <hello@axeladrian.com>
2025-04-09 09:43:52 +0700

    feat(code): introduce Code component with customizable properties and rendering logic; add Script function for syntax highlighting and copy functionality; update templates to utilize new Code component

--- helpers/helpers_templ.go
+++ helpers/helpers_templ.go
@@ -12,6 +12,7 @@ import (
 	"github.com/axzilla/templui/components"
 	"github.com/axzilla/templui/components/carousel"
 	"github.com/axzilla/templui/components/chart"
+	"github.com/axzilla/templui/components/code"
 )
 
 func ComponentScripts() templ.Component {
@@ -43,7 +44,7 @@ func ComponentScripts() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = components.CodeScript().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = code.Script().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
