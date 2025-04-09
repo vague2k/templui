f01c193b3113f9a4c214c24a3cbdcf28982c033e
axzilla <hello@axeladrian.com>
2025-04-09 15:32:37 +0700

    feat(slider): introduce Slider component with customizable properties and templates; update showcases and references to utilize the new Slider implementation

--- helpers/helpers_templ.go
+++ helpers/helpers_templ.go
@@ -22,6 +22,7 @@ import (
 	"github.com/axzilla/templui/components/popover"
 	"github.com/axzilla/templui/components/rating"
 	"github.com/axzilla/templui/components/selectbox"
+	"github.com/axzilla/templui/components/slider"
 )
 
 func ComponentScripts() templ.Component {
@@ -93,7 +94,7 @@ func ComponentScripts() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = components.SliderScript().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = slider.Script().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
