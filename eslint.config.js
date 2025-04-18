22b192b42b22c94ceae94fcf60c6aea051875452
Diana Suvorova <diana.suvorova@gmail.com>
2025-04-18 08:43:59 -0700

    chore: adding alpine and tailwind linters

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 const htmlPlugin = require("@html-eslint/eslint-plugin");
 const eslintHTMLParser = require("@html-eslint/parser");
+const alpinejs = require("eslint-plugin-alpinejs");
+const tailwind = require("eslint-plugin-html-tailwind");
 
 module.exports = [
   {
@@ -9,9 +11,14 @@ module.exports = [
     },
     plugins: {
       "@html-eslint": htmlPlugin,
+      alpinejs,
+      "html-tailwind": tailwind,
     },
     rules: {
       ...htmlPlugin.configs.recommended.rules,
+      ...alpinejs.configs.recommended.rules,
+      ...tailwind.configs.recommended.rules,
+      "html-tailwind/classname-order": "off",
     },
   },
 ];
